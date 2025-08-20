f5d7bb744b5f1df5b0a8872d598dd5214c267a8b
mengjihua <1684882284@qq.com>
2025-08-20 13:37:00 +0800

    添加每日一题(动态规划), 修正并优化 other\import.py 文件

--- 每日一题/2025.8.4后/1277.py
+++ 每日一题/2025.8.4后/1277.py
@@ -13,3 +13,16 @@ input = lambda: stdin.readline().rstrip()
 def _max(a, b): return a if a > b else b
 def _min(a, b): return a if a < b else b
 MOD = 10 ** 9 + 7
+
+class Solution:
+    # 学会做定点找动态方程
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        ans = 0
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if matrix[i - 1][j - 1] == 1:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                    ans += dp[i][j]
+        return ans
