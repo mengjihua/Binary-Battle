cefc19aab59b037b3c9023732b0d5b394ff1b38c
mengjihua <1684882284@qq.com>
2025-09-09 10:01:47 +0800

    差分数组 优化时间复杂度

--- 每日一题/2025.9/2327.py
+++ 每日一题/2025.9/2327.py
@@ -25,3 +25,19 @@ class Solution:
                 dp[j] += dp[i]
 
         return sum(dp[-forget:]) % MOD
+    
+    def peopleAwareOfSecret1(self, n: int, delay: int, forget: int) -> int:
+        '''使用差分数组优化, 时间复杂度 O(n)'''
+        diff = [0] * (n + 1)
+        diff[1], diff[2] = 1, -1
+        ans = known = 0
+        for i in range(1, n + 1):
+            known = (known + diff[i]) % MOD
+            if i >= n - forget + 1:
+                ans += known
+            if i + delay <= n:
+                diff[i + delay] = (diff[i + delay] + known) % MOD
+            if i + forget <= n:
+                diff[i + forget] = (diff[i + forget] - known) % MOD
+                
+        return ans % MOD
