a65b254b027804d0769caf991a3da2ac46bbfd48
mengjihua <1684882284@qq.com>
2025-09-25 11:29:26 +0800

    每日一题 -> dp

---
+++ 每日一题/2025.9/120.py
@@ -0,0 +1,32 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, accumulate
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sortedcontainers import SortedList
+from sys import setrecursionlimit, stdin, stdout
+setrecursionlimit(5 * 10 ** 5 + 1)
+input = lambda: stdin.readline().rstrip()
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+MOD = 10 ** 9 + 7
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n, m = len(triangle), len(triangle[-1])
+        dp = [[inf] * m for _ in range(n)]
+        dp[0][0] = triangle[0][0]
+        for i in range(1, n):
+            for j in range(i + 1):
+                if j == 0:
+                    dp[i][j] = dp[i - 1][j] + triangle[i][j]
+                elif j == i:
+                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j]
+                else:
+                    dp[i][j] = fmin(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
+        return min(dp[-1])
