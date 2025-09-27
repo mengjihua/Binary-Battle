949dfa0c14ef0b9b0a4b49a6a3fea251a9a4a48d
mengjihua <1684882284@qq.com>
2025-09-27 17:40:34 +0800

    每日一题, 待补进阶方法

---
+++ 每日一题/2025.9/812.py
@@ -0,0 +1,35 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, accumulate
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sortedcontainers import SortedList
+from sys import setrecursionlimit, stdin, stdout
+setrecursionlimit(5 * 10 ** 5 + 1)
+input = lambda: stdin.readline().rstrip()
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+MOD = 10 ** 9 + 7
+
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        n = len(points)
+        ans = 0
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                for k in range(j + 1, n):
+                    x3, y3 = points[k]
+                    area = abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2
+                    ans = fmax(ans, area)
+        return ans
+    
+    def largestTriangleArea(self, points: List[List[int]]) -> floor:
+        # TODO: 待选择(凸包)
+        pass
