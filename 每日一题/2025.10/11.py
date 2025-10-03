0f2703c6688648d490a6c0f3e14e16387b15830d
mengjihua <1684882284@qq.com>
2025-10-04 00:59:40 +0800

    每日一题 -> 双指针

---
+++ 每日一题/2025.10/11.py
@@ -0,0 +1,30 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sys import setrecursionlimit, stdin, stdout
+from random import getrandbits
+setrecursionlimit(5 * 10 ** 4 + 1)
+input = lambda: stdin.readline().rstrip()
+RD = getrandbits(31)
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        ans = 0
+        l, r = 0, len(height) - 1
+        while l < r:
+            ans = max(ans, (r - l) * min(height[l], height[r]))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return ans
--- 每日一题/2025.10/407.py
+++ 每日一题/2025.10/407.py
@@ -8,9 +8,8 @@ from math import gcd, sqrt, log, ceil, floor, inf
 from bisect import bisect_left, bisect_right
 from heapq import heappush, heappop, heapify, nsmallest, nlargest
 from sortedcontainers import SortedList
-from sys import setrecursionlimit, stdin, stdout
+from sys import setrecursionlimit
 setrecursionlimit(5 * 10 ** 5 + 1)
-input = lambda: stdin.readline().rstrip()
 def fmax(a, b): return a if a > b else b
 def fmin(a, b): return a if a < b else b
 def lcm(a, b): return a * b // gcd(a, b)
