f13c7935c44e461969420d47ac47f65ea934fe68
mengjihua <1684882284@qq.com>
2025-11-01 10:51:03 +0800

    每日一题 -> 哈希访问

---
+++ 每日一题/2025.10/3217.py
@@ -0,0 +1,34 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, accumulate, pairwise
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, comb, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sortedcontainers import SortedList
+from sys import setrecursionlimit
+setrecursionlimit(5 * 10 ** 5 + 1)
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+MOD = 10 ** 9 + 7
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        st = set(nums)
+        cur = dummy = ListNode(next=head)
+        while cur.next:
+            nxt = cur.next
+            if nxt.val in st:
+                cur.next = nxt.next
+            else:
+                cur = nxt
+        return dummy.next
---
+++ 每日一题/2025.10/3289.py
@@ -0,0 +1,36 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, accumulate, pairwise
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, comb, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sortedcontainers import SortedList
+from sys import setrecursionlimit
+setrecursionlimit(5 * 10 ** 5 + 1)
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+MOD = 10 ** 9 + 7
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        cnt = Counter(nums)
+        ans = []
+        for cnt, val in cnt.items():
+            if val != 1:
+                ans.append(cnt)
+        return ans
+    
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        vis = [False] * len(nums)
+        ans = []
+        for num in nums:
+            if vis[num]:
+                ans.append(num)
+                vis[num] = False
+            else:
+                vis[num] = True
+        return ans
