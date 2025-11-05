4e4912c7f3ca5f2c8e4bae1886bbf93b7533ff53
mengjihua <1684882284@qq.com>
2025-11-05 19:33:56 +0800

    每日一题

---
+++ 每日一题/2025.11/3318.py
@@ -0,0 +1,31 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, accumulate, pairwise
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, comb, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sortedcontainers import SortedList
+from sys import setrecursionlimit
+setrecursionlimit(5 * 10 ** 5 + 1)
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+MOD = 10 ** 9 + 7
+
+
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        n = len(nums)
+        ans = []
+        for i in range(n - k + 1):
+            sub = nums[i:i + k]
+            st = set(sub)
+            if len(st) < x:
+                ans.append(sum(sub))
+            else:
+                cnt = sorted(Counter(sub).items(), key=lambda x: (x[0], x[1]), reverse=True)
+                ans.append(sum(c * v for c, v in cnt[:x]))
+        return ans
