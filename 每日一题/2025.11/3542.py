89064d52d15e535ae3fd37e03ad7e5e3ac4b31aa
mengjihua <1684882284@qq.com>
2025-11-10 19:38:23 +0800

    每日一题 -> 单调栈

---
+++ 每日一题/2025.11/3542.py
@@ -0,0 +1,28 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, accumulate, pairwise
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, comb, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sortedcontainers import SortedList
+from sys import setrecursionlimit
+setrecursionlimit(5 * 10 ** 5 + 1)
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+MOD = 10 ** 9 + 7
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        ans = 0
+        st = []
+        for num in nums:
+            while st and num < st[-1]:
+                st.pop()
+                ans += 1
+            if not st or num != st[-1]:
+                st.append(num)
+        return ans + len(st) - (st[0] == 0)
