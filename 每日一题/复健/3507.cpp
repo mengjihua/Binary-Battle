cc3aeff1c21cf2a3b2f72ea794ed04e436606146
mengjihua <1684882284@qq.com>
2026-01-23 17:08:51 +0800

    订正补全3507C++代码逻辑

--- 每日一题/复健/3507.cpp
+++ 每日一题/复健/3507.cpp
@@ -5,7 +5,7 @@ const int MOD = 1e9 + 7;
 class Solution {
 public:
     int minimumPairRemoval(vector<int>& nums) {
-        int n = 0, ans = 0;
+        int ans = 0;
         while (true) {
             int n = nums.size();
 
@@ -26,6 +26,7 @@ public:
                     idx = i;
                 }
             }
+            nums[idx] = nums[idx] + nums[idx + 1];
             nums.erase(nums.begin() + idx + 1);
             ans += 1;
         }
