258ffc97a08e2f6401e2766903c95e997e71e8c1
mengjihua <1684882284@qq.com>
2026-03-03 16:50:49 +0800

    2026-Offer-Expedition-Campaign Week 1: Mock Interview I

---
+++ 模拟面试/Ⅰ/Q1.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, accumulate, pairwise
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, comb, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sortedcontainers import SortedList
+from sys import setrecursionlimit
+setrecursionlimit(5 * 10 ** 5 + 1)
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+MOD = 10 ** 9 + 7
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        if not any(i == 0 for i in nums): return
+        i = nums.index(0)
+        for j in range(i + 1, n):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
---
+++ 模拟面试/Ⅰ/Q3.py
@@ -0,0 +1,43 @@
+from typing import List, Tuple, Dict, Set, Optional
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, accumulate, pairwise
+from datetime import datetime, date, time, timedelta
+from time import time as timestamp, sleep
+from functools import cmp_to_key, lru_cache, reduce
+from math import gcd, comb, sqrt, log, ceil, floor, inf
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, nsmallest, nlargest
+from sortedcontainers import SortedList
+from sys import setrecursionlimit
+setrecursionlimit(5 * 10 ** 5 + 1)
+def fmax(a, b): return a if a > b else b
+def fmin(a, b): return a if a < b else b
+def lcm(a, b): return a * b // gcd(a, b)
+MOD = 10 ** 9 + 7
+
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        dp = [0] * (n + 1)
+        dp[1] = 1
+
+        for i in range(1, n + 1):
+            for j in range(i + delay, fmin(i + forget, n + 1)):
+                dp[j] += dp[i]
+
+        return sum(dp[-forget:]) % MOD
+    
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        diff = [0] * (n + 2)
+        diff[1], diff[2] = 1, -1
+        ans = now = 0
+        
+        for i in range(1, n + 1):
+            now = now + diff[i]
+            if i >= n - forget + 1:
+                ans += now
+            if i + delay <= n:
+                diff[i + delay] += now
+            if i + forget <= n:
+                diff[i + forget] -= now
+                
+        return ans % MOD
